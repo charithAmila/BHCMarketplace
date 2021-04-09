6d056e343ec731008b3c6ae9fc1b72c2295417e7
punsisikiridana <punsisikiridana@gmail.com>
2021-04-09 15:11:27 +0530

    commit

--- database/migrations/2021_01_08_115617_create_followers_table.php
+++ database/migrations/2021_01_08_115617_create_followers_table.php
@@ -15,10 +15,9 @@ class CreateFollowersTable extends Migration
     {
         Schema::create('followers', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('follower_id');
+            $table->string('user_id');
+            $table->string('follower_id');
             $table->boolean('followed')->default(false);
-            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
