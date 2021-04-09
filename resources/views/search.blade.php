0600771dcc6630980436ff114ff0d72e3b15ee7b
punsisikiridana <punsisikiridana@gmail.com>
2021-04-09 16:56:00 +0530

    js

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,10 +3,9 @@
 @section('extra_css')
 
 
-<search-page :query="{{ json_encode($query) }}" :csrf_token="{{ json_encode(csrf_token()) }}"
-    :collectible_asset="'collectible_aasest'" :show_collectible="{{ json_encode(asset('/nft').'/') }}"
-    :base_url="{{ json_encode(URL::to('/')) }}" :asset_url="{{ json_encode(asset('/')) }}"
-    :marketplace_url="{{ json_encode(route('marketplace')) }}"></search-page>
+<search-page :query="{{ json_encode($query) }}" :csrf_token="{{ json_encode(csrf_token()) }}" :collectible_asset="'collectible_aasest'" :show_collectible="{{ json_encode(asset('/nft').'/') }}" :base_url="{{ json_encode(URL::to('/')) }}" :asset_url="{{ json_encode(asset('/')) }}" :marketplace_url="{{ json_encode(route('marketplace')) }}"></search-page>
+
+
 @section('extra_scripts')
 <script src="{{ asset('js/custom.js') }}"></script>
 @endsection
