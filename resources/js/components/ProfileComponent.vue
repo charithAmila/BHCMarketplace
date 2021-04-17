6b2a3e98990a8a6be9da5f62bd0097ff6fe1610c
Iresh Madhusankha <52518140+crezix@users.noreply.github.com>
2021-04-17 18:07:15 +0530

    debug

--- resources/js/components/ProfileComponent.vue
+++ resources/js/components/ProfileComponent.vue
@@ -1,117 +1,132 @@
 <template>
-  <div>
-    <div class="all-collection">
-      <div class="filter-collection">
-        <a
-          id="on-sale"
-          class="filter active"
-          href="javascript:void(0)"
-          @click="filterProfileNft('on-sale')"
-          >On sale</a
-        >
-        <a
-          id="collectibles"
-          class="filter"
-          href="javascript:void(0)"
-          @click="filterProfileNft('collectibles')"
-          >Collectibles</a
-        >
-        <a
-          id="created"
-          class="filter"
-          href="javascript:void(0)"
-          @click="filterProfileNft('created')"
-          >Created</a
-        >
-        <a
-          id="liked"
-          class="filter"
-          href="javascript:void(0)"
-          @click="filterProfileNft('liked')"
-          >Liked</a
-        >
-        <a
-          id="fetchFollowing"
-          class="following"
-          href="javascript:void(0)"
-          @click="getFollowing()"
-          >Following</a
-        >
-        <a class="follower" href="javascript:void(0)" @click="getFollower()"
-          >Followers</a
-        >
-      </div>
-      <div class="collection-tab">
-        <div id="preloader" class="row grid-conatainer" v-if="loading">
-          <div
-            v-for="index in 12"
-            :key="index"
-            class="col-md-3 col-lg-3 custom-column-xl main-dashboard"
-          >
-            <div class="outside-nft border-on-profile">
-              <div class="inside-nft">
-                <div class="inner-outside-nft">
-                  <div class="inner-nft">
-                    <div class="item-main">
-                      <div class="item-head">
-                        <div class="preloader-img"></div>
-                      </div>
-                    </div>
-
-                    <div class="item-img"></div>
-
-                    <div class="display-flex -mt-15">
-                      <div class="preloader-content"></div>
-                    </div>
-                    <div class="text-center currency-label">
-                      <div class="preloader-content"></div>
-                    </div>
-                    <div class="preloader-half">
-                      <div class="preloader-content"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+    <div>
+        <div class="all-collection">
+            <div class="filter-collection">
+                <a
+                    id="on-sale"
+                    class="filter active"
+                    href="javascript:void(0)"
+                    @click="filterProfileNft('on-sale')"
+                    >On sale</a
+                >
+                <a
+                    id="collectibles"
+                    class="filter"
+                    href="javascript:void(0)"
+                    @click="filterProfileNft('collectibles')"
+                    >Collectibles</a
+                >
+                <a
+                    id="created"
+                    class="filter"
+                    href="javascript:void(0)"
+                    @click="filterProfileNft('created')"
+                    >Created</a
+                >
+                <a
+                    id="liked"
+                    class="filter"
+                    href="javascript:void(0)"
+                    @click="filterProfileNft('liked')"
+                    >Liked</a
+                >
+                <a
+                    id="fetchFollowing"
+                    class="following"
+                    href="javascript:void(0)"
+                    @click="getFollowing()"
+                    >Following</a
+                >
+                <a
+                    class="follower"
+                    href="javascript:void(0)"
+                    @click="getFollower()"
+                    >Followers</a
+                >
             </div>
-          </div>
+            <div class="collection-tab">
+                <div id="preloader" class="row grid-conatainer" v-if="loading">
+                    <div
+                        v-for="index in 12"
+                        :key="index"
+                        class="col-md-3 col-lg-3 custom-column-xl main-dashboard"
+                    >
+                        <div class="outside-nft border-on-profile">
+                            <div class="inside-nft">
+                                <div class="inner-outside-nft">
+                                    <div class="inner-nft">
+                                        <div class="item-main">
+                                            <div class="item-head">
+                                                <div
+                                                    class="preloader-img"
+                                                ></div>
+                                            </div>
+                                        </div>
+
+                                        <div class="item-img"></div>
+
+                                        <div class="display-flex -mt-15">
+                                            <div
+                                                class="preloader-content"
+                                            ></div>
+                                        </div>
+                                        <div class="text-center currency-label">
+                                            <div
+                                                class="preloader-content"
+                                            ></div>
+                                        </div>
+                                        <div class="preloader-half">
+                                            <div
+                                                class="preloader-content"
+                                            ></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <collectible-component
+                    :div_id="div_id"
+                    :collectible_asset="collectible_asset"
+                    :show_collectible="show_collectible"
+                    :current_user="current_user"
+                    :base_url="base_url"
+                    :collectibles="showing"
+                    :page="'profile'"
+                    :filter="filter"
+                    :user_id="user_id"
+                ></collectible-component>
+
+                <following-modal-component
+                    :following="followings"
+                    :asset_url="asset_url"
+                ></following-modal-component>
+
+                <follower-modal-component
+                    :followers="followers"
+                    :asset_url="asset_url"
+                ></follower-modal-component>
+
+                <div
+                    v-if="showing.length == 0 && !loading"
+                    class="no-item-found w-30"
+                >
+                    <span class="sad-face">:(</span>
+                    <span class="collection-title">No items found</span>
+                    <span class="collection-desc"
+                        >Come back soon! Or try to browse something for you on
+                        our marketplace</span
+                    >
+                    <a class="browse" :href="marketplace_url"
+                        >Browse marketplace</a
+                    >
+                </div>
+            </div>
         </div>
-
-        <collectible-component
-          :div_id="div_id"
-          :collectible_asset="collectible_asset"
-          :show_collectible="show_collectible"
-          :current_user="current_user"
-          :base_url="base_url"
-          :collectibles="showing"
-          :page="'profile'"
-          :filter="filter"
-          :user_id="user_id"
-        ></collectible-component>
-
-        <following-modal-component
-          :following="followings"
-          :asset_url="asset_url"
-        ></following-modal-component>
-
-        <follower-modal-component
-          :followers="followers"
-          :asset_url="asset_url"
-        ></follower-modal-component>
-
-        <div v-if="showing.length == 0 && !loading" class="no-item-found w-30">
-          <span class="sad-face">:(</span>
-          <span class="collection-title">No items found</span>
-          <span class="collection-desc"
-            >Come back soon! Or try to browse something for you on our
-            marketplace</span
-          >
-          <a class="browse" :href="marketplace_url">Browse marketplace</a>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
-
 
 <script>
 import $ from "jquery";
@@ -119,136 +134,155 @@ import Collectible from "./CollectibleComponent.vue";
 import Following from "./modals/FollowingModalComponent.vue";
 import Follower from "./modals/FollowerModalComponent.vue";
 import {
-  getTokensData,
-  getOnSaleTokens,
-  getLikedTokens,
-  getCreatedTokens,
+    getTokensData,
+    getOnSaleTokens,
+    getLikedTokens,
+    getCreatedTokens
 } from "./../data.js";
 import { getUserDetails } from "../data";
 import { log } from "util";
 export default {
-  components: {
-    Collectible,
-    Following,
-    Follower,
-  },
-  props: [
-    "collectible_asset",
-    "show_collectible",
-    "current_user",
-    "base_url",
-    "asset_url",
-    "marketplace_url",
-    "user_slug",
-    "user_id",
-  ],
-  data() {
-    return {
-      div_id: "collectible-field",
-      rowClass: true,
-      collectibles: {
-        "on-sale": [],
-        collectibles: [],
-        liked: [],
-        created: [],
-      },
-      showing: [],
-      filter: "on-sale",
-      following: [],
-      loading: false,
-      followers: [],
-      followings: [],
-      output_array: [],
-      output_array_followings: [],
-    };
-  },
-  methods: {
-    async getCollectible() {
-      const _this = this;
-      _this.loading = true;
-      var data = await getTokensData(_this.user_id, _this.base_url);
-      _this.collectibles = data;
-      _this.collectibles.liked = [];
-      _this.collectibles.created = [];
-      _this.showing = _this.collectibles[_this.filter];
-      _this.loading = false;
+    components: {
+        Collectible,
+        Following,
+        Follower
     },
-    async filterProfileNft(selectedFilter) {
-      const _this = this;
-      _this.loading = true;
-      _this.filter = selectedFilter;
-      _this.showing = _this.collectibles[selectedFilter];
-      if (selectedFilter == "on-sale") {
-        var onSale = await getOnSaleTokens(_this.user_id, _this.base_url);
-        _this.collectibles[selectedFilter] = onSale;
-      } else if (
-        selectedFilter == "liked" &&
-        _this.collectibles[selectedFilter].length == 0
-      ) {
-        var liked = await getLikedTokens(_this.user_id, _this.base_url);
-        _this.collectibles[selectedFilter] = liked;
-      } else if (
-        selectedFilter == "created" &&
-        _this.collectibles[selectedFilter].length == 0
-      ) {
-        var created = await getCreatedTokens(_this.user_id, _this.base_url);
-        _this.collectibles[selectedFilter] = created;
-      }
-      _this.showing = _this.collectibles[selectedFilter];
-      _this.loading = false;
+    props: [
+        "collectible_asset",
+        "show_collectible",
+        "current_user",
+        "base_url",
+        "asset_url",
+        "marketplace_url",
+        "user_slug",
+        "user_id"
+    ],
+    data() {
+        return {
+            div_id: "collectible-field",
+            rowClass: true,
+            collectibles: {
+                "on-sale": [],
+                collectibles: [],
+                liked: [],
+                created: []
+            },
+            showing: [],
+            filter: "on-sale",
+            following: [],
+            loading: false,
+            baseLoaded: false,
+            followers: [],
+            followings: [],
+            output_array: [],
+            output_array_followings: []
+        };
     },
-    refreshAfterUpdate() {
-      this.getCollectible();
-    },
-    getFollowing() {
-      modalOpen($("#following-modal"), $(".following-content"));
-    },
-    getFollower() {
-      modalOpen($("#follower-modal"), $(".follower-content"));
-    },
-    async getFollowers() {
-      var user_id = this.user_id;
-      var _this = this;
-      var res = await axios.get("/followers");
-      _this.output_array = res.data.followers.filter(function (elem) {
-        if (elem.user_id == user_id) {
-          return elem.user_id;
-        }
-      });
-    },
-    async getFollowings() {
-      var user_id = this.user_id;
-      const new_user_id = user_id.toLowerCase();
-      var _this = this;
-      var res = await axios.get("/followers");
-      _this.output_array_followings = res.data.followers.filter(function (
-        elem
-      ) {
-        if (elem.follower_id == new_user_id) {
-          return elem.follower_id;
-        }
-      });
-    },
-    async asignFollowers() {
-      var _this = this;
-      for (var i = 0; i < this.output_array.length; i++) {
-        var resu = await getUserDetails(this.output_array[i].follower_id);
-        _this.followers.push(resu);
-      }
+    methods: {
+        async getCollectible() {
+            const _this = this;
+            _this.loading = true;
+            var data = await getTokensData(_this.user_id, _this.base_url);
+            _this.collectibles = data;
+            _this.collectibles.liked = [];
+            _this.collectibles.created = [];
+            _this.showing = _this.collectibles[_this.filter];
+            _this.loading = false;
+            _this.baseLoaded = true;
+        },
+        async filterProfileNft(selectedFilter) {
+            const _this = this;
+            _this.loading = true;
+            _this.filter = selectedFilter;
+            _this.showing = _this.collectibles[selectedFilter];
+            if (selectedFilter == "on-sale") {
+                var onSale = await getOnSaleTokens(
+                    _this.user_id,
+                    _this.base_url
+                );
+                _this.collectibles[selectedFilter] = onSale;
+            } else if (
+                selectedFilter == "liked" &&
+                _this.collectibles[selectedFilter].length == 0
+            ) {
+                var liked = await getLikedTokens(_this.user_id, _this.base_url);
+                _this.collectibles[selectedFilter] = liked;
+            } else if (
+                selectedFilter == "created" &&
+                _this.collectibles[selectedFilter].length == 0
+            ) {
+                var created = await getCreatedTokens(
+                    _this.user_id,
+                    _this.base_url
+                );
+                _this.collectibles[selectedFilter] = created;
+            }
+            _this.showing = _this.collectibles[selectedFilter];
+            if (this.baseLoaded) {
+                _this.loading = false;
+            }
+        },
+        refreshAfterUpdate() {
+            this.getCollectible();
+        },
+        getFollowing() {
+            modalOpen($("#following-modal"), $(".following-content"));
+        },
+        getFollower() {
+            modalOpen($("#follower-modal"), $(".follower-content"));
+        },
+        async getFollowers() {
+            var user_id = this.user_id;
+            var _this = this;
+            var res = await axios.get("/followers");
+            _this.output_array = res.data.followers.filter(function(elem) {
+                if (elem.user_id == user_id) {
+                    return elem.user_id;
+                }
+            });
+        },
+        async getFollowings() {
+            var user_id = this.user_id;
+            const new_user_id = user_id.toLowerCase();
+            var _this = this;
+            var res = await axios.get("/followers");
+            _this.output_array_followings = res.data.followers.filter(function(
+                elem
+            ) {
+                if (elem.follower_id == new_user_id) {
+                    return elem.follower_id;
+                }
+            });
+        },
+        async asignFollowers() {
+            var _this = this;
+            for (var i = 0; i < this.output_array.length; i++) {
+                var resu = await getUserDetails(
+                    this.output_array[i].follower_id
+                );
+                _this.followers.push(resu);
+            }
 
-      for (var j = 0; j < this.output_array_followings.length; j++) {
-        var resul = await getUserDetails(
-          this.output_array_followings[j].user_id
-        );
-        _this.followings.push(resul);
-      }
+            for (var j = 0; j < this.output_array_followings.length; j++) {
+                var resul = await getUserDetails(
+                    this.output_array_followings[j].user_id
+                );
+                _this.followings.push(resul);
+            }
+        }
     },
-  },
-  async mounted() {
-    await this.getFollowers();
-    await this.getFollowings();
-    await this.asignFollowers();
-    await this.getCollectible();
-  },
+    async mounted() {
+        await this.getFollowers();
+        await this.getFollowings();
+        await this.asignFollowers();
+        this.loading = true;
+        await this.getCollectible();
+        this.loading = false;
+    },
+    watch: {
+        filter() {
+            //this.loading = true;
+        }
+    }
 };
 </script>
--- resources/js/components/pages/CollectiblePage.vue
+++ resources/js/components/pages/CollectiblePage.vue
@@ -1,68 +1,72 @@
 <template>
-  <div>
-    <div v-if="loaded">
-      <show-collectible-component
-        :collectible="collectible"
-        :onWishList="onWishList"
-        :transactions="transactions"
-        :is_liked="is_liked"
-        :asset_url="asset_url"
-        :auth_check="is_auth"
-        :user_profile="user_profile"
-        :base_url="base_url"
-      ></show-collectible-component>
+    <div>
+        <div v-if="loaded">
+            <show-collectible-component
+                :collectible="collectible"
+                :onWishList="onWishList"
+                :transactions="transactions"
+                :is_liked="is_liked"
+                :asset_url="asset_url"
+                :auth_check="is_auth"
+                :user_profile="user_profile"
+                :base_url="base_url"
+            ></show-collectible-component>
 
-      <show-fullscreen-component
-        :collectible="collectible"
-        :asset_url="asset_url"
-      ></show-fullscreen-component>
+            <show-fullscreen-component
+                :collectible="collectible"
+                :asset_url="asset_url"
+            ></show-fullscreen-component>
+        </div>
+        <div v-else>
+            <cube></cube>
+            <h2 class="text-center">NFT is loading....</h2>
+        </div>
     </div>
-  </div>
 </template>
 <script>
 import { getTokenData } from "./../../data";
 import { checkConnection, getOwnersOf } from "./../../etherFunc";
 export default {
-  props: ["asset_url", "user_profile", "base_url", "contract", "owner", "id"],
-  data() {
-    return {
-      is_liked: false,
-      is_auth: false,
-      onWishList: false,
-      collectible: { creator: "dddd" },
-      transactions: [],
-      loaded: false,
-      current_user: "",
-      owners: [],
-    };
-  },
-  methods: {
-    getCollectible: async function () {
-      const _this = this;
-      var data = await getTokenData(
-        _this.contract,
-        _this.owner,
-        Number(_this.id)
-      );
+    props: ["asset_url", "user_profile", "base_url", "contract", "owner", "id"],
+    data() {
+        return {
+            is_liked: false,
+            is_auth: false,
+            onWishList: false,
+            collectible: { creator: "dddd" },
+            transactions: [],
+            loaded: false,
+            current_user: "",
+            owners: []
+        };
+    },
+    methods: {
+        getCollectible: async function() {
+            const _this = this;
+            var data = await getTokenData(
+                _this.contract,
+                _this.owner,
+                Number(_this.id)
+            );
 
-      return data;
+            return data;
+        },
+        getOwners: async function() {
+            const _this = this;
+            var _owners = await getOwnersOf(_this.contract, _this.id);
+            _this.collectible.owners = _owners;
+        },
+        authCheck: function() {
+            const _this = this;
+            _this.current_user = checkConnection();
+            _this.current_user == _this.owner ? (_this.is_auth = true) : null;
+        }
     },
-    getOwners: async function () {
-      const _this = this;
-      var _owners = await getOwnersOf(_this.contract, _this.id);
-      _this.collectible.owners = _owners;
-    },
-    authCheck: function () {
-      const _this = this;
-      _this.current_user = checkConnection();
-      _this.current_user == _this.owner ? (_this.is_auth = true) : null;
-    },
-  },
-  async mounted() {
-    this.collectible = await this.getCollectible();
-    await this.getOwners();
-    this.authCheck();
-    this.loaded = true;
-  },
+    async mounted() {
+        this.collectible = await this.getCollectible();
+        await this.getOwners();
+        this.authCheck();
+        this.loaded = true;
+    }
 };
 </script>
