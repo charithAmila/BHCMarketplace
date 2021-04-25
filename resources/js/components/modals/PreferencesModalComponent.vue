e3e75e774fa700a80a87f170508726c325b57c96
punsisikiridana <punsisikiridana@gmail.com>
2021-04-25 19:44:11 +0530

    short_url

--- resources/js/components/modals/PreferencesModalComponent.vue
+++ resources/js/components/modals/PreferencesModalComponent.vue
@@ -87,7 +87,7 @@
                             </div>
                             <div class="form-divide">
                                 <label class="input-label">Short url</label>
-                                <span class="link-url">billionhappiness.finance/profile/</span>
+                                <span class="link-url">profile/</span>
                                 <input
                                     id="short_url-profile"
                                     class="modal-input preferences-check with-link"
