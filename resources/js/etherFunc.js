298e3a0abc7798fc2c9f2f2fc583fdc6a6c8f09d
Iresh Madhusankha <52518140+crezix@users.noreply.github.com>
2021-04-25 21:11:17 +0530

    up

--- resources/js/etherFunc.js
+++ resources/js/etherFunc.js
@@ -41,8 +41,8 @@ if (typeof window.ethereum == "undefined") {
 //window.rpcprovider1 = new ethers.providers.JsonRpcProvider(
 //"https://apis.ankr.com/90ca2e28d7af47eea5a0d41b1236d19d/10acafa95fd982713d5972bad68960fc/binance/full/main"
 //"https://data-seed-prebsc-1-s1.binance.org:8545"
-//"http://162.0.210.42/rpc"
-//);
+// "http://162.0.210.42/rpc"
+////);
 
 //const selectedAddress = provider.provider.selectedAddress;
 
@@ -183,12 +183,12 @@ async function getCollection(collectionAddess) {
                 latest.data.length == 0 ? 6494200 : latest.data.block + 1;
             var endBlock = await rpcprovider.getBlockNumber();
             var evts = [];
-            for (var i = startBlock; i <= endBlock; i = i + 2000) {
+            for (var i = startBlock; i <= endBlock; i = i + 4000) {
                 var transfers = [];
                 var evtsCr = await contract.queryFilter(
                     "TransferSingle",
                     i,
-                    i + 2000
+                    i + 4000
                 );
                 for (var x = 0; x < evtsCr.length; x++) {
                     var event = evtsCr[x];
@@ -226,6 +226,7 @@ async function getCollection(collectionAddess) {
 
 async function getCreated(owner, _startingBlock) {
     const tokens = [];
+    window.myTokens.created = [];
 
     try {
         const nftStorage = new ethers.Contract(
@@ -277,12 +278,13 @@ async function getCreated(owner, _startingBlock) {
         var evts = [];
         console.log(startBlock);
 
-        for (var i = startBlock; i <= endBlock; i = i + 2000) {
+        for (var i = startBlock; i <= endBlock; i = i + 4000) {
             var st = i;
+            await axios.patch("/minted/" + owner, { block: i - 1 });
             var evtsCr = await nftStorage.queryFilter(
                 "NFTAdded",
                 i,
-                i + 2000 <= endBlock ? i + 2000 : endBlock
+                i + 4000 <= endBlock ? i + 4000 : endBlock
             );
             console.log(evtsCr);
             var mints = [];
@@ -336,7 +338,7 @@ async function getCreated(owner, _startingBlock) {
             }
         }
     } catch (e) {
-        //await getCreated(owner, st);
+        await getCreated(owner, st);
         console.log(e);
     }
     if (window.myTokens.created.length == 0) {
@@ -423,13 +425,13 @@ async function getOwnersOf(collectionAddess, tokenId, _startBlock) {
                 null,
                 null
             );
-            for (var i = startBlock; i <= endBlock; i = i + 2000) {
+            for (var i = startBlock; i <= endBlock; i = i + 4000) {
                 var st = i;
                 var transfers = [];
                 var evtsCr = await contract.queryFilter(
                     "TransferSingle",
                     i,
-                    endBlock >= i + 2000 ? i + 2000 : endBlock
+                    endBlock >= i + 4000 ? i + 4000 : endBlock
                 );
                 for (var x = 0; x < evtsCr.length; x++) {
                     var event = evtsCr[x];
@@ -513,12 +515,12 @@ async function getAnOwner(collectionAddess, tokenId, _startBlock, _owner) {
                 null,
                 null
             );
-            for (var i = startBlock; i <= endBlock; i = i + 2000) {
+            for (var i = startBlock; i <= endBlock; i = i + 4000) {
                 var st = i;
                 var evtsCr = await contract.queryFilter(
                     "TransferSingle",
                     i,
-                    i + 2000
+                    i + 4000
                 );
                 console.log(evtsCr);
 
