aceaeb95d6f7835b499a29455dc71fdd3bd06799
punsisikiridana <punsisikiridana@gmail.com>
2021-04-26 15:57:24 +0530

    social

--- resources/js/components/SocialLinkComponent.vue
+++ resources/js/components/SocialLinkComponent.vue
@@ -4,10 +4,10 @@
 			<a href="https://twitter.com/bhc_happiness" target="_blank">
 				<img :src="asset_url+'images/twitter.png'"> Follow us on Twitter
 			</a>
-			<a href="https://t.me/BillionHappinessOfficial" target="_blank">
+			<a href="https://t.me/BH_NFT_Support" target="_blank">
 				<img :src="asset_url+'images/telegram.png'"> Join our Telegram community
 			</a>
-			<a href="javascript:void(0)">
+			<a href="https://rbjrcxhzb4w.typeform.com/to/Ak0nEWnn" target="_blank">
 				<img :src="asset_url+'images/message.png'"> Community Feedback
 			</a>
 		</div>
