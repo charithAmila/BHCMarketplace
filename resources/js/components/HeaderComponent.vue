2d31039db056e572c8689fc0c794de7b699ed053
John Paul Montilla <yoqweqw143@gmail.com>
2021-04-29 17:22:08 +0800

    hide scroll

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -217,7 +217,7 @@
         <a :href="marketplace_route">Marketplace</a>
         <a :href="profile_route + '/' + user_link">My NFT's</a>
         <a :href="faq_route">FAQ</a>
-        <a :href="about_route">HPS</a>
+        <a :href="about_route">BHC</a>
       </div>
       <div class="menu-footer">
         <div class="footer-social">
