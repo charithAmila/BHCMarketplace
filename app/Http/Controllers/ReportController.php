b04411be60ec065790d20a8d3e001cefb3b71585
punsisikiridana <punsisikiridana@gmail.com>
2021-05-07 21:37:01 +0530

    report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -30,7 +30,7 @@ class ReportController extends Controller
 
     public function show(Request $request,$contract,$token_id)
     {
-    $data = Report::where('contract',$contract)->where('token_id',$token_id)->where('reported',true)->first();
+    $data = Report::where('contract',$contract)->where('token_id',$token_id)->first();
     return $data;
     
 }
